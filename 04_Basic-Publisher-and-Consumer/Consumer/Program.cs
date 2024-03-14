a9b318240b2af44f1682d6d8829ab93a4c137655
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-03-14 16:30:21 +0300

    gy lecture 4, basic publisher and consumer

---
+++ 04_Basic-Publisher-and-Consumer/Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+/* Order of Operations for Consumer Application */
+
+// Creating Connection 
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+
+// Activating Connection and Opening Channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Creating Queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false); // The queue in the consumer should be configured exactly the same as in the publisher.
+
+// Reading Messages from Queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: "example-queue",
+    autoAck: true,
+    consumer: consumer
+    );
+
+consumer.Received += (sender, e) =>
+{
+    // Place where the received message from the queue is processed.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+};
+
+Console.Read();
---
+++ 04_Basic-Publisher-and-Consumer/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+/* Order of Operations for Publisher Application */
+
+// Creating Connection
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+//factory.Uri = new("amqps://..."); => If working on the Cloud.
+
+// Activating Connection and Opening Channel
+using IConnection connection = factory.CreateConnection(); // Using 'using' to optimize memory since IConnection is IDisposable.
+using IModel channel = connection.CreateModel();
+
+// Creating Queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Sending Messages to Queue
+
+// RabbitMQ accepts messages to be sent to the queue as bytes, so messages need to be converted to bytes.
+
+//byte[] message = Encoding.UTF8.GetBytes("Hello!");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message); // Default exchange applied (direct exchange)
+
+for (int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i + 1}");
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
